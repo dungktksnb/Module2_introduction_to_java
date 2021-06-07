227ca89a61a3598ab1cd00a02b2759c8f6bd1a09
dungktksnb <dungcnttk14g@gmail.com>
2021-06-07 16:59:41 +0700

    bai1

--- bai1/src/Main.java
+++ bai1/src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("bạn hãy nhập tên của mình :");
+        String name=scanner.nextLine();
+        System.out.println("hello " +name);
 
 
     }
