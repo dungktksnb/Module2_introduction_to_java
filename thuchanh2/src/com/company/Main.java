80cfc169603582bd90dbb15e7d6b8b0191a8b8b4
dungktksnb <dungcnttk14g@gmail.com>
2021-06-07 09:04:02 +0700

    th2

---
+++ thuchanh2/src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int i=10;
+	float f=20.5f;
+	double d=20.5;
+	boolean b=true;
+	char c='a';
+	String s="hà nội";
+        System.out.println("biến i kiểu số nguyên có giá trị là:"+i);
+        System.out.println("biến f kiểu số thực có giá trị là :" +f);
+        System.out.println("biến d kiểu số thực lớn có giá trị là:" +d);
+        System.out.println("biến b kiểu logic có giá trị là :" +b);
+        System.out.println("biến c kiểu kí tự có giá trị là:" +c);
+        System.out.println("biến s kiểu chuỗi có giá trị là :" +s);
+
+    }
+}
